34174a91b9c76ca600a9fee95f6d44a2906c6746
Praveen Gupta <pkgdc74@gmail.com>
2018-01-25 02:21:14 -0500

    NS

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -20,31 +20,36 @@ export class SettingsPage {
   conInfo: ConnectionInfo = {
     url: "https://cadapultfm1.cadapult.local/fmcloudbeta", cid: "FMDev", userid: "", password: ""
   }
-  constructor(public navCtrl: NavController, private ds: DataService,private toastCtrl:ToastController) {
-    ds.get("connectionSetting").then((x)=>{
-      if(x==null)return
-        this.conInfo=x;
-        this.conInfo.password=this.conInfo.password.d()
+  constructor(public navCtrl: NavController, private ds: DataService, private toastCtrl: ToastController) {
+    ds.get("connectionSetting").then((x) => {
+      if (x == null) return
+      this.conInfo = x;
+      this.conInfo.password = this.conInfo.password.d()
     })
   }
 
   connect() {
+    let options = { message: "", duration: 3000 };
     var rmis = new cfm.rmi.RMIService()
     rmis.setRMIHeader({ cid: this.conInfo.cid, userid: this.conInfo.userid, password: this.process(this.conInfo.password) })
     rmis.getProxyAsync("com.mobile.invpmdm.InvPMDMRMIService", `${this.conInfo.url}/invpmdm/mobile/invpmdmmobilermiservice.asp`)
       .then((proxy) => {
-        proxy.testConnectionAsync().then((x) => { 
-          console.log(x.status)
-          let info=Object.assign({},this.conInfo)
-          info.password=info.password.e();
-          this.ds.set("connectionSetting",info)
-          this.ds.startDataEngine()
-          let toast = this.toastCtrl.create({
-            message: 'connected',
-            duration: 3000
-          });
-          toast.present()
+        proxy.testConnectionAsync().then((x) => {
+          if (x.status === "OK") {
+            let info = Object.assign({}, this.conInfo)
+            info.password = info.password.e();
+            options.message = "Connection successful"
+            this.toastCtrl.create(options).present()
+            this.ds.set("connectionSetting", info).then(()=>this.ds.startDataEngine());
+          } else {
+            options.message = x.msg
+            this.toastCtrl.create(options).present()
+          }
         })
+      },
+      (x) => {
+        options.message = x.msg
+        this.toastCtrl.create(options).present()
       })
   }
 
@@ -54,13 +59,13 @@ export class SettingsPage {
     return password
   }
 
-  validate(x: string,pass:string): boolean {
+  validate(x: string, pass: string): boolean {
     let time = x.split("-")
     if (new Date().getTime() > Number(time[0])) return false;
     return md5(time[0] + pass) == time[1]
   }
 
   toggleMode(i) {
-    
+
   }
 }
