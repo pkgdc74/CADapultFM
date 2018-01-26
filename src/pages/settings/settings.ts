778ed1e3d491dfb14d48e3bf173889e37d31df98
Praveen Gupta <pkgdc74@gmail.com>
2018-01-26 01:30:34 -0500

    NS

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, ToastController } from 'ionic-angular';
 import { DataService } from '../../providers/data-service/data-service';
 
-declare var xxx:any;
+declare var cfm:any;
 
 export interface ConnectionInfo {
   url: string,
@@ -22,7 +22,7 @@ export class SettingsPage {
   }
 
   constructor(public navCtrl: NavController, private ds: DataService, private toastCtrl: ToastController) {
-    alert(xxx)
+    alert(cfm)
     ds.get("connectionSetting").then((x) => {
       if (x == null) return
       this.conInfo = x;
