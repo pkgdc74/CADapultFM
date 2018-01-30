d89a7481b73e2dac3c7831912acf5469ddca6d20
pkgdc74 <pkgdc74@gmail.com>
2018-01-30 17:08:25 -0500

    NS

--- src/pages/dm/dmtasks.ts
+++ src/pages/dm/dmtasks.ts
@@ -15,7 +15,7 @@ export class DMTasksPage {
   constructor(public navCtrl: NavController, private modalCtrl: ModalController, private ds: DataService) {}
 
   ionViewWillEnter() {
-    this.subscription=this.ds.subscribe("DMTasks", (d:object[]) => { this.wos = d;})
+    this.subscription=this.ds.subscribe("DMTasks", (d:object[]) => { this.wos=this.wos.concat(d)})
   }
   
   ionViewWillLeave() {
@@ -32,17 +32,27 @@ export class DMTasksPage {
     } else {
       wo.startTime = new Date().getTime()
     }
+    this.saveState()
   }
 
   closeWO(wo) {
+    this.saveState()
   }
-  
+
+  saveState(){
+    this.ds.get("wos").then(x=>{
+      x.DMTasks=this.wos;
+      this.ds.set("wos",x)
+    })
+  }
   
   signWO(wo) {
     let m: Modal = this.modalCtrl.create(SignaturePage, { du: wo.signature || "" })
     m.onDidDismiss((d) => {
-      if (!wo.signature)
+      if (!wo.signature){
         wo.signature = d;
+        this.saveState()
+      }
     })
     m.present()
   }
--- src/providers/data-service.ts
+++ src/providers/data-service.ts
@@ -5,61 +5,92 @@ import { BehaviorSubject } from "rxjs/BehaviorSubject";
 import { Observable } from "rxjs/Rx";
 import { Security } from "./security";
 import { Subscription } from 'rxjs/Subscription';
+
 declare var cfm;
 
 @Injectable()
 export class DataService {
-  private publisher:BehaviorSubject<any>;
-  private engine:Observable<any>;
-  private subscription={unsubscribe:()=>{}}
+  private publisher: BehaviorSubject<any>;
+  private engine: Observable<any>;
+  private subscription = { unsubscribe: () => { } }
 
-  constructor(private storage:Storage,private security:Security) {
-    this.engine=Observable.timer(0,10000)
-    this.publisher=new BehaviorSubject(0)
+  constructor(private storage: Storage, private security: Security) {
+    this.engine = Observable.timer(0, 300000)
+    this.publisher = new BehaviorSubject(0)
+    this.get("wos").then(x=>{
+      this.publisher.next(x)
+    })
     this.appStateChanged()
   }
-  
-  get(key:string):Promise<any>{
-    return this.storage.get(key).then((d)=>{
-      d=d==null?null:JSON.parse(d)
+
+  get(key: string): Promise<any> {
+    return this.storage.get(key).then((d) => {
+      d = d == null ? null : JSON.parse(d)
       return d;
-    }).catch(d=>{
+    }).catch(d => {
       console.log(d)
     })
   }
 
-  set(key:string,data:any):Promise<any>{
-    return this.storage.set(key,JSON.stringify(data))
+  set(key: string, data: any): Promise<any> {
+    return this.storage.set(key, JSON.stringify(data))
   }
 
-  subscribe(name:string,x:(a:object[])=>void):Subscription{
-    return this.publisher.subscribe((data)=>{
-        if(data==0)return
-        x(data[name])
+  subscribe(name: string, x: (a: object[]) => void): Subscription {
+    return this.publisher.subscribe((data) => {
+      if (data == 0) return
+      x(data[name])
     })
   }
 
-  woSummary(x:(a:{})=>void){
-    return this.publisher.subscribe((data)=>{
-      if(data==0)return
-      data={"dm":data["DMTasks"].length,"pm":data["PMTasks"].length,"assets":data["Assets"].length}
+  woSummary(x: (a: {}) => void) {
+    return this.publisher.subscribe((data) => {
+      if (data == 0) return
+      data = { "dm": data["DMTasks"].length, "pm": data["PMTasks"].length, "assets": data["Assets"].length }
       x(data)
     })
   }
-
-  appStateChanged(){
-    this.get("connectionSetting").then((info)=>{
-      if(info==null)return
-      if(info.status==1 && !info.offline){
+  private syncData(local:any[],remote:any[]):any[]{
+    local.forEach(x=>{
+      let i=remote.findIndex(y=>{
+        console.log(x.requestid==y.requestid)
+        return x.requestid==y.requestid
+      })
+      if(i!==-1)
+        remote.splice(i,1)
+    })
+    return remote
+  }
+  appStateChanged() {
+    this.get("connectionSetting").then((info) => {
+      if (info == null) return
+      if (info.status == 1 && !info.offline) {
         this.subscription.unsubscribe();
-        this.subscription=this.engine.switchMap((x)=>{
-          let rmi=new cfm.rmi.RMIService()
-          rmi.setRMIHeader({cid:info.cid,userid:info.userid,password:this.security.authtoken(info.password.d())});
-          let p=rmi.getProxyAsync("com.mobile.invpmdm.InvPMDMRMIService", `${info.url}/invpmdm/mobile/invpmdmmobilermiservice.asp`)
-          .then(x=>x.gettechWOAsync())
+        this.subscription = this.engine.switchMap((x) => {
+          let rmi = new cfm.rmi.RMIService()
+          rmi.setRMIHeader({ cid: info.cid, userid: info.userid, password: this.security.authtoken(info.password.d()) });
+          let p = rmi.getProxyAsync("com.mobile.invpmdm.InvPMDMRMIService", `${info.url}/invpmdm/mobile/invpmdmmobilermiservice.asp`)
+          .then(x => x.gettechWOAsync())
+          .then(remote=>{
+            return this.get("wos").then((local)=>{
+              local=local||{DMTasks:[],PMTasks:[],Assets:[]}
+              let sdata = {
+                DMTasks:this.syncData(local.DMTasks,remote.DMTasks),
+                PMTasks:remote.PMTasks,
+                Assets:remote.Assets
+              }
+              local.DMTasks=local.DMTasks.concat(sdata.DMTasks)
+              this.set("wos",local)
+              return sdata;
+            })
+          })
           return Observable.fromPromise(p)
-        }).subscribe(x=>this.publisher.next(x))
-      }else{
+        })
+        .subscribe(x => {
+          if(x.DMTasks.length==0 && x.PMTasks.length==0 && x.Assets.length==0)return
+          this.publisher.next(x);
+        })
+      } else {
         this.subscription.unsubscribe();
       }
     })
